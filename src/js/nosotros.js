f3cd03e363f26915beb7d926236e884ed61b0348
Jonathan Hernandez <jonathanxav.informatica@gmail.com>
2024-04-11 13:24:37 -0600

    ci: se agrega carrusel de desarrolladores y se arregla bug con el mismo

---
+++ src/js/nosotros.js
@@ -0,0 +1,134 @@
+const samplePersonal = [{
+  'id_personal':'1',
+  'nombre_persona': 'Alejandro Montoya Torres',
+  'link_linkedln': ' https://www.linkedin.com/in/alejandromontoyadeveloper/ ',
+  'img':"Alejandro Montoya Torres..jpeg",
+  'descripcion_p': ` ¡Hola!, Soy Alejandro Montoya Torres, ingeniero de sistemas computacionales egresado del TECHA apasionado por la tecnología con entusiasmo de poder saber cómo funcionan los sistemas y electrónicos, de tal manera me ha orillado a desarrollar mis habilidades en la programación y poder crear y materializar ideas extraordinarias con herramientas asombrosas. `
+},
+{
+  'id_personal':'2',
+  'nombre_persona': 'Uriel Sergio Chávez Barajas',
+  'link_linkedln': ' https://www.linkedin.com/in/uriel-schavez/ ',
+  'img':"Uriel Chávez.jpeg",
+  'descripcion_p':` Soy Arquitecto y diseñador mexicano, chilango, amante de las jacarandas y la ciudad, me ha apasionado desde temprana edad poder materializar y representar mis ideas de manera plástica y visual. Esta misma inquietud me ha orillado actualmente a aprender el diseño y desarrollo de páginas y aplicaciones web en el curso de Desarrollador Java Full Stack en Generation. `
+},
+{
+  'id_personal':'3',
+  'nombre_persona': 'Adriana Montserrat Flores Bacilio',
+  'link_linkedln': ' https://www.linkedin.com/in/adriana-bacilioing/ ',
+  'img':"Adriana Montserrat Flores Bacilio.jpeg",
+  'descripcion_p':`¡Hola!, Soy Adriana Bacilio nací el 26 de julio de 1998 en Guadalajara Jalisco MéxicoEstudié Ingeniería Mecatrónica con especialidad en robótica en el Centro de Enseñanza Técnica Industrial, actualmente estudio para ser Desarrolladora de Java FullStack en Generation Mx. `
+},
+{
+  'id_personal':'4',
+  'nombre_persona': 'Yuritzy Nicol Martínez Segoviano',
+  'link_linkedln': ' https://www.linkedin.com/in/ymartinezs/ ',
+  'img':"Yuritzy Nicol Martínez Segoviano.jpeg",
+  'descripcion_p':` ¡Hola!, Soy Yuritzy Nicol Martínez Segoviano, Nacida en Ciudad de Mexico, tengo una carrera Técnica en Procesos Industriales y soy Egresada de la carrera de Ingeniera Petrolera en la ESIA Ticoman. Actualemente estoy en proceso de formación de aprendizaje en desarrollo full stack Java. `
+},
+{
+  'id_personal':'5',
+  'nombre_persona': 'Abraham jimenez Valdes',
+  'link_linkedln': ' https://www.linkedin.com/in/ingabrahamjv/ ',
+  'img':"fotodeperfil.jpg",
+  'descripcion_p':` ¡Hola!, Soy Brand. Soy Ingeniero en Gestión de Sistemas por la Universidad de Guanajuato, Licenciado en Derecho  Penal por la UAM y Licenciado en Administración de Empresas por la UNAM. Tengo una amplia experiencia laboral en                   corporativos. Mis gustos son muy variados pero simples: me encanta ver series, jugar juegos de estrategia o simuladores, y pasar tiempo con mis perros. Disfruto del silencio y de los pequeños placeres de la vida. `
+},
+{
+  'id_personal': '6',
+  'nombre_persona': 'Dámaris Silva Rayas',
+  'link_linkedln': ' https://www.linkedin.com/in/damaris-silva-r-0a7ba5177/ ',
+  'img': "Dámaris Silva Rayas.jpeg",
+  'descripcion_p': ` ¡Hola!, Soy Dámaris Silva Rayas Nací un invierno de 1996. Tengo una ingeniería en informática y una Maestría en Ciencia de Datos dónde me he desempeñado en el campo de la analítica de los datos para empresas internacionales como mexicanas. Actualmente estoy en un bootcamp de Generation México aprendiendo lo que es ser un desarrollador Full Stack al mismo tiempo estudio una nueva licenciatura en el IBF. `
+},
+{
+  'id_personal': '7',
+  'nombre_persona': 'Edgar Hernández González',
+  'link_linkedln': ' https://www.linkedin.com/in/edgar-hdz-glez/',
+  'img': "Edgar Hernández González.jpeg",
+  'descripcion_p': ` ¡Hola!, Soy Edgar Hernández González Nací el 7 de Diciembre de 1993 en Naucalpan, Estado de México, viví algunos años en Guadalajara, regrese a CDMX y estudie la Licenciatura en Informática Administrativa en Universidad CNCI, trabaje un año en Caliente Casino, actualmente estoy cursando el bootcamp de Desarrollo Java Fullstack en Generation y en proceso de mudanza a Guadalajara. `
+}, 
+{
+  'id_personal': '8',
+  'nombre_persona': 'Javier Díaz Villagómez',
+  'link_linkedln': ' https://www.linkedin.com/in/villagomezjavieraz/ ',
+  'img': "javier diaz  villagomez.jpeg",
+  'descripcion_p': ` ¡Hola!, Soy Javier Díaz Villagómez. Oaxaqueño y aventurero. Soy ingeniero en computación por parte del IPN. Entre mis pasiones está el entrenamiento multifuncional. Me considero apasionado y entregado en lo que tiene mi atención. Actualmente formo parte del programa de formación Generation México con el objetivo de ser un programador Full Stack. `
+},
+{
+  'id_personal': '9',
+  'nombre_persona': 'Cristian Jonathan Juárez Ruiz',
+  'link_linkedln': ' https://www.linkedin.com/in/cjonathanjuarezr/ ',
+  'img': "Cristian Jonathan Juárez Ruiz.jpeg",
+  'descripcion_p': ` ¡Hola!, Soy Cristian Jonathan Juárez Ruiz Nací el 23 de noviembre de 1994. Crecí en Naucalpan, Estado de México con mis padres (María y Miguel) y mis hermanos (Miguel y Uriel). Estudié Ciencias de la Comunicación en el Centro de Estudios Superiores en Ciencias Jurídicas y Criminológicas y he trabajado en el medio durante 7 años. Actualmente estudio Desarrollo Java Fullstack en Generation y me emociona (aunque también me atemoriza) descubrir todo un mundo nuevo en el desarrollo profesional. `
+ },
+ {
+  'id_personal': '10',
+  'nombre_persona': 'Jonathan Xavier Hernández Ortega',
+  'link_linkedln': 'https://www.linkedin.com/in/jonathan-xavier-hernandez-ortega-95431b221/',
+  'img': "Jonathan Xavier Hernández Ortega.jpeg",
+  'descripcion_p': ` ¡Hola!, Soy Jonathan Xavier Hernández Ortega Nacido en 1996 en Alvaro Obregon me crie por la zona, asisti al kínder privado Alexander Dull los primeros 2 años, Posteriormente termine el kinder n el Lauro Aguirre, curse primer y parte del  segundo año de primaria en la Guadalupe Victoria pero posteriormente fui transferido a la Primaria Zambia donde termine(y sufri) mis peores años, curse toda la secundaria en la Diurna 170 Heberto Castillo donde gracias a un amigo elegi como primer opcion al bachillerato el CeCYT 5 "Benito Juarez" los 3 mejores años de mi vida Finalmente conclui mis estudios en la Unidad Profesional Interdisciplinaria de Ciencias Sociales y Administrativas UPIICSA Finalmente Trabaje 1 año en ELAESI como Gerente de Informatica y Desarrollador Full-stack y 1 año en BBVA primero como externo 5 meses y despues 6 meses de interno. `
+}];
+localStorage.setItem("items", JSON.stringify(samplePersonal));
+
+
+const agregarPersonal = () => {
+  const storageItems = localStorage.getItem("items")
+  if (storageItems) {
+      const card = document.getElementById("card-personal");
+      const items = JSON.parse(storageItems); 
+      
+          items.forEach(item => {
+            if(item.id_personal === '1')
+            {
+              card.innerHTML += `
+                <div class="carousel-item active text-center">
+            <img src="../../assets/img/${item.img}" class="img-fluid img-thumbnail" id="img-personal" alt="..." style="max-height: 550px;">
+            <div class="carousel-caption d-md-block">
+              <h5>${item.nombre_persona}</h5>
+              <a href="${item.link_linkedln}"><img src="../../assets/img/linkdin-logo.png"  style="width:30px; background-color:White; border-radius:.5rem;"></a>
+              <button type="button" id="persona${item.id_personal}" class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Biografia
+              </button>
+              </div>
+            </div>
+              `;
+            }
+            else{
+              card.innerHTML += `
+                <div class="carousel-item text-center">
+                  <img src="../../assets/img/${item.img}" class="img-fluid img-thumbnail" alt="..." style="max-height: 550px;">
+                  <div class="carousel-caption d-md-block">
+                    <h5>${item.nombre_persona}</h5>
+                    <a href="${item.link_linkedln}"><img src="../../assets/img/linkdin-logo.png" style="width:30px; background-color:White; border-radius:.5rem;"></a>
+                    <button type="button" id="persona${item.id_personal}" class=" btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                      Biografia
+                    </button>
+                  </div>
+                </div>
+              `;
+            }   
+          });
+  }
+}
+
+
+$(function() {
+  $(document).on('click', 'button[type="button"]', function(event) {
+     const storageItems = localStorage.getItem("items")
+     if (storageItems) {
+         const card = document.getElementById("texto-modal");
+         const items = JSON.parse(storageItems); 
+         
+             items.forEach(item => {
+               if ("persona"+item.id_personal === this.id) {
+                 card.innerHTML = `
+                   <p>${item.descripcion_p}</p>
+                 `;  
+               } 
+             }); 
+                 
+     }
+   });
+ });
+
+agregarPersonal();
