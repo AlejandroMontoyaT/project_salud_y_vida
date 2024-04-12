b43a99466fee596df706fd940924d8cd67510fea
Jonathan Hernandez <jonathanxav.informatica@gmail.com>
2024-04-12 08:25:56 -0600

    fix: se arreglan varios elementos de las paginas asi como bugs que impedian navegacion en modo celular

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,11 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var carouselItems = document.querySelectorAll("#carouselExampleIndicators .carousel-item");
+import '../../styles.scss';
 
-  var currentIndex = 0; 
-  function nextItem() {
-      carouselItems[currentIndex].classList.remove("active");
-      currentIndex = (currentIndex + 1) % carouselItems.length;
-      carouselItems[currentIndex].classList.add("active");
-  }
-  var interval = setInterval(nextItem, 15000);
-});
